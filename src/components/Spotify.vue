ca741a9eee357d3cb398ad1d3053dfd075610487
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-02-11 14:49:37 +0000

    Commented out chart.css graphs

--- src/components/Spotify.vue
+++ src/components/Spotify.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="content article-body">
     {{ content }}
-    <Graph style="margin-top: 30px;"/>
+<!--    <Graph style="margin-top: 30px;"/>-->
   </div>
 </template>
 
 <script>
-import Graph from "@/components/Graph";
+// import Graph from "@/components/Graph";
 export default {
   name: "Spotify",
   props: {
     content: String
   },
   components: {
-    Graph
+    // Graph
   }
 }
 </script>

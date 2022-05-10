e8d65f7a4221ff3ecf649a334041b6efe4f62b74
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-05-10 15:01:26 +0100

    fix(github): stats URI

--- src/services/github/main.js
+++ src/services/github/main.js
@@ -6,14 +6,14 @@ const Cache = require("../../utils/cache");
 const cache = new Cache();
 
 const getStats = function() {
-    const url = "https://github-stats-rest.vercel.app/api?username=leo-pfeiffer&count_private=true&show_icons=true"
+    const url = "https://github-stats-rest.vercel.app/api?username=leo-pfeiffer&count_private=true"
     return axios.get(url)
         .then((res) => res.data)
         .catch((err) => console.error(err))
 }
 
 const getLangs = function() {
-    const url = "https://github-stats-rest.vercel.app/api/top-langs/?username=leo-pfeiffer&layout=compact&hide=Jupyter%20Notebook,HTML,CSS&exclude_repo=Functional-Programming-in-Scala"
+    const url = "https://github-stats-rest.vercel.app/api/top-langs/?username=leo-pfeiffer&hide=TeX,Jupyter%20Notebook,HTML,CSS,SCSS&exclude_repo=Functional-Programming-in-Scala,solidvest"
     return axios.get(url)
         .then((res) => res.data)
         .catch((err) => console.error(err))

5c064a5c09d55ac9eef20ef973172d9c7faa1b55
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-05-10 12:54:30 +0100

    feat(github): github card first draft

--- src/components/Github.vue
+++ src/components/Github.vue
@@ -1,112 +1,118 @@
 <template>
   <div class="content article-body">
-    <div class="level box">
+    <div class="level box is-mobile">
       <div class="level-item has-text-centered">
         <div>
-          <p class="heading">Period (dys)</p>
-          <p class="title">30</p>
+          <p class="heading" :title="`Commits (${year})`">
+            <svg class="icon" viewBox="0 0 16 16"><path :d="icons.commits.d"/></svg>
+          </p>
+          <p class="title">{{ githubStats.totalCommits }}</p>
         </div>
       </div>
       <div class="level-item has-text-centered">
         <div>
-          <p class="heading">Total Time (hrs)</p>
-          <p class="title">123</p>
+          <p class="heading" title="Stars">
+            <svg class="icon" viewBox="0 0 16 16"><path :d="icons.star.d"/></svg>
+          </p>
+          <p class="title">{{ githubStats.totalStars }}</p>
         </div>
       </div>
       <div class="level-item has-text-centered">
         <div>
-          <p class="heading">Daily Average (hrs)</p>
-          <p class="title">123</p>
+          <p class="heading" title="Pull requests">
+            <svg class="icon" viewBox="0 0 16 16"><path :d="icons.prs.d"/></svg>
+          </p>
+          <p class="title">{{ githubStats.totalPRs }}</p>
         </div>
       </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading" title="Issues">
+            <svg class="icon" viewBox="0 0 16 16"><path :d="icons.issues.d"/></svg>
+          </p>
+          <p class="title">{{ githubStats.totalIssues }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="box">
+      <apexchart type="bar" width="100%" height="200" :options="chartOptions" :series="githubLanguages"></apexchart>
     </div>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-// import VueC3 from 'vue-c3'
-// import wakatime from "@/api/wakatime";
 import github from "@/api/github";
-// import ColorScheme from 'color-scheme';
-// const scheme = new ColorScheme();
-
+import icons from "@/assets/icons";
 
 export default {
   name: "WakaTime",
   props: {
     content: String
   },
-  // components: {
-  //   VueC3
-  // },
   data() {
     return {
-      editorChartHandler: new Vue(),
-      langChartHandler: new Vue(),
-      wakaData: {},
+      githubStats: {totalPRs: 0, totalCommits: 0, totalIssues: 0, totalStars: 0},
+      githubLanguages: [],
       available: false,
+
+      chartOptions: {
+        chart: {
+          type: 'bar',
+          stacked: true,
+          stackType: '100%'
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true,
+          },
+        },
+        stroke: {
+          width: 1,
+          colors: ['#fff']
+        },
+        title: {text: 'Top Languages'},
+        xaxis: {categories: [""],},
+        tooltip: {enabled: false,},
+        fill: {
+          opacity: 1
+        },
+        legend: {
+          position: 'top',
+          horizontalAlign: 'left',
+          offsetX: 0
+        }
+      },
     }
   },
   computed: {
-    totalTime: function() {
-       return this.wakaData.total / 3600
+    year: function() {
+      return new Date().getFullYear()
     },
-    avgTime: function() {
-      return this.wakaData.average / 3600
-    },
+    icons: function() {
+      return icons;
+    }
   },
   methods: {
-    makeChart: function(columnData, handler, scheme) {
-      const options = {
-        size: {
-          height: 200,
-        },
-        data: {
-          columns: columnData,
-          type: 'donut',
-          empty: {
-            label: {
-              text: "No Data Available"
-            }
-          }
-        },
-        color: {
-          pattern: scheme.colors().map(e => '#' + e)
-        },
-        donut: {
-          label: {
-            show: false,
-          },
-        },
-        legend: {
-          show: true
-        },
-      }
-      handler.$emit('init', options);
-    },
     round: function (num, d) {
       const m = Math.pow(10, d)
       return Math.floor(num * m) / m;
     },
+    processLangs: function(langs) {
+      const runs = Object.entries(langs)
+          .map(e => e[1])
+          .sort((a, b) => b["size"] - a["size"])
+      console.log(runs)
+
+      return runs.slice(0, 5)
+          .map(e => {
+            return {"name": e.name, "data": [e.size]}
+          })
+    }
   },
   async mounted() {
-    const data = await github.retrieve()
-    console.log(data)
-    // if (Object.keys(data).length > 0) {
-    //   this.wakaData = data
-    //   this.available = true
-    //
-    //   const editors = this.wakaData.editors.reduce((a, b) => [... a, [b.name, b.total]], [])
-    //   const editorScheme = scheme.from_hex('00faff').scheme('contrast');
-    //   this.makeChart(editors, this.editorChartHandler, editorScheme);
-    //
-    //   let langs = this.wakaData.languages.reduce((a, b) => [... a, [b.name, b.total]], [])
-    //   langs.sort((a, b) => b[1] > a[1] ? 1 : -1)
-    //   langs = langs.slice(0, Math.min(5, langs.length))
-    //   const langScheme = scheme.from_hex('cb21ff').scheme('contrast');
-    //   this.makeChart(langs, this.langChartHandler, langScheme);
-    // }
+    const githubData = await github.retrieve();
+    this.githubStats = githubData["stats"]
+    this.githubLanguages = this.processLangs(githubData["languages"])
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'charts.css';
+import VueApexCharts from 'vue-apexcharts'
+
+Vue.use(VueApexCharts)
+Vue.component('apexchart', VueApexCharts)
 
 Vue.config.productionTip = false
 

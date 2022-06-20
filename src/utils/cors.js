b665f136e191e5581ff84e0a626974ea8fe73bd3
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-06-20 22:34:34 +0100

    fix: add missing cors headers

--- src/utils/cors.js
+++ src/utils/cors.js
@@ -1,9 +1,7 @@
 const cors_headers = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-    'Content-Type': 'application/json',
+    'Access-Control-Allow-Headers': '*',
     'Access-Control-Allow-Methods': '*',
-    'Access-Control-Max-Age': '2592000',
     'Access-Control-Allow-Credentials': 'true',
 };
 

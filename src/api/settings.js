c7ab4132fda1aaa717396801c2919d9c13800f78
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-10-22 18:01:28 +0100

    chore: update pase url

--- src/api/settings.js
+++ src/api/settings.js
@@ -1,6 +1,10 @@
-const BASE_URL = "https://glasshouse-backend.netlify.app"
+const VUE_APP_BASE_URL = "https://glasshouse.netlify.app"
 // const BASE_URL = "http://localhost:8888"
+// const VUE_APP_BASE_URL = process.env.VUE_APP_BASE_URL
+
+console.log("HARD CODED", VUE_APP_BASE_URL)
+console.log("BASE_URL", process.env.BASE_URL)
 
 module.exports = {
-    BASE_URL
+    BASE_URL: VUE_APP_BASE_URL
 }

1252f97448d1a9883f7a73734818dba8e435841f
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-10-22 18:25:25 +0100

    refactor: remove unused methods from cache wrapper

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,9 +1,7 @@
 const NodeCache = require( "node-cache" );
 
 /**
- * Exposes a wrapper for a caching mechanism. Right now, node-cache is used
- * but the wrapper would allow future use of something more sophisticated
- * such as Redis.
+ * Exposes a caching wrapper.
  * */
 module.exports = class {
 
@@ -17,47 +15,11 @@ module.exports = class {
         this.provider.set(key, val, ttl)
     }
 
-    mset(arr) {
-        this.provider.mset(arr);
-    }
-
     get(key) {
         return this.provider.get(key)
     }
 
-    take(key) {
-        return this.provider.take(key)
-    }
-
-    mget(keys) {
-        return this.provider.mget(keys)
-    }
-
-    del(key) {
-        this.provider.del(key)
-    }
-
-    mdel(keys) {
-        this.provider.mdel(keys)
-    }
-
-    ttl(key, ttl) {
-        this.provider.ttl(key, ttl)
-    }
-
-    getTtl(key) {
-        return this.provider.getTtl(key)
-    }
-
-    keys() {
-        return this.provider.keys()
-    }
-
     has(key) {
         return this.provider.has(key)
-    }
-
-    flushAll() {
-        this.provider.flushAll()
     }
 }

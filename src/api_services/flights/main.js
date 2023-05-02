8afe3c94314964b5e836e32171dc040e57569229
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2023-05-02 15:02:28 +0100

    feat: Add api endpoint for flights

--- src/api_services/flights/main.js
+++ src/api_services/flights/main.js
@@ -8,16 +8,16 @@ const getFlights = function() {
     return axios.get(FLIGHT_URL, { responseType: 'blob'})
         .then((res) => res.data)
         .then((res) => csvToJson(res))
-        .then((csv) => console.log(csv))
         .catch((err) => console.error(err))
 }
 
 const getAirportData = function(iataCode) {
     return axios.get(AIRPORT_URL + iataCode)
         .then((res) => res.data)
-        .then((json) => console.log(json))
         .catch((err) => console.error(err))
 }
 
-getFlights()
-getAirportData("EDI")
+module.exports = {
+    getFlights: getFlights,
+    getAirportData: getAirportData
+};
